131d08aec5a538a71f66a7dfdec7cede66fd30e5
Sean Zhang <sean7218@gmail.com>
2025-06-28 18:36:30 -0400

    renamed it and added some README.md

--- Package.swift
+++ Package.swift
@@ -4,19 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "view-indexdb",
+    name: "isdb-view",
+    products: [
+        .executable(name: "isdb-view", targets: ["isdb-view"])
+    ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/indexstore-db.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.1")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "view-indexdb",
+            name: "isdb-view",
             dependencies: [
                 .product(name: "IndexStoreDB", package: "indexstore-db"),
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ]),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        )
     ]
 )
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# IndexstoreDBViewer
+# IndexstoreDBView (isdb-view)
 
+A simple CLI that you can list all the index-db symbols
+
+```
+swift run isdb-view list-symbols \
+    --store-path /path/to/.index-store \
+    --database-path /path/to/.index-db \
+    --library-path /path/to/libIndexStore.dylib
+```
 
 1. depend on (indexstore-db)[https://github.com/swiftlang/indexstore-db] to view the database
